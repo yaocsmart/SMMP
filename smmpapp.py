35a779c3ccbfcf63dcff33ff88d9ef20743d8adf
yaocsmart <yaocsmart@163.com>
2026-03-27 08:20:51 +0800

    Update smmpapp.py

--- smmpapp.py
+++ smmpapp.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import shap
 import matplotlib.pyplot as plt
+import shap
 import joblib
 import os
 new_directory = "E:/CAI1229/cai0226data/SMMP"
