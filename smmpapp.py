2e4eee56f5ff6c3c43f93adf77e33b78e42279fb
yaocsmart <yaocsmart@163.com>
2026-04-03 23:41:10 +0800

    Update smmpapp.py

--- smmpapp.py
+++ smmpapp.py
@@ -18,7 +18,9 @@ model_path = current_dir / "GBDT.pkl"
 model = None
 try:
     # 直接用joblib加载（你的模型是sklearn原生，无需自定义Unpickler）
-    model = joblib.load(model_path)
+    # model = joblib.load(model_path)
+    with open("GBDT.pkl", 'rb') as f:
+    model = pickle.load(f)
     st.success(f"✅ 模型加载成功！路径：{model_path}")
 except Exception as e:
     st.error(f"❌ 模型加载失败！错误信息：{str(e)}")
